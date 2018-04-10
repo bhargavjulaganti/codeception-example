6307b6965ca46606b89505a66c5067cd6d6021bd
bhargavjulaganti <bhargavjulaganti@gmail.com>
2018-04-10 13:45:14 -0400

    Update WelcomeCept.php
    modified the WelcomCept.php

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -1,8 +1,14 @@
 <?php
-$capabilities = $scenario->current('capabilities');
-codecept_debug($capabilities);
-$I = new AcceptanceTester($scenario);
-$I->wantTo('ensure that frontpage works');
-$I->amOnPage('/');
-$I->seeInTitle('Google');
+
+$a=5;
+
+if($a==5){
+    echo "in if loop \n ";
+}
+//$capabilities = $scenario->current('capabilities');
+//codecept_debug($capabilities);
+//$I = new AcceptanceTester($scenario);
+//$I->wantTo('ensure that frontpage works');
+//$I->amOnPage('/');
+//$I->seeInTitle('Google');
 ?>
